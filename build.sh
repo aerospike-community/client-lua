ee55d3169a4eee99265f517dbfe0916f5186f887
peter <peter@aerospike.com>
2014-03-10 11:06:12 -0500

    added build.sh

---
+++ build.sh
@@ -0,0 +1,2 @@
+gcc -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"src/as_lua.d" -MT"src/as_lua.d" -o "./src/as_lua.o" "src/as_lua.c" -std=gnu99 -g -rdynamic -Wall -fno-common -fno-strict-aliasing -fPIC -DMARCH_x86_64 -D_FILE_OFFSET_BITS=64 -D_REENTRANT -D_GNU_SOURCE -DMEM_COUNT
+gcc -shared -o "as_lua.so"  ./src/as_lua.o -laerospike -lssl -lcrypto -lpthread -lrt -llua -lm
